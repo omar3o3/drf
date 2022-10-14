3d3e2672d0ac79a28f69796bbcb72ffa5ad22bd1
omar3o3 <otantawy53@gmail.com>
2022-10-13 22:47:23 -0400

    returing data from db from api_home method

---
+++ py_client/basic.py
@@ -0,0 +1,12 @@
+import requests
+
+endpoint = 'http://localhost:8000/api/'
+
+# get_response = requests.get(endpoint)
+# print(get_response.text)
+# print(get_response.json()['message'])
+# print(get_response.status_code)
+# print(get_response)
+
+get_response = requests.get(endpoint, params={'abc': 123}, json={'query': "Hello world!"})
+print(get_response.json())
