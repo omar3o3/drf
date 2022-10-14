8f362200eedb8a360f91885e867097542d2b3e5e
omar3o3 <otantawy53@gmail.com>
2022-10-14 00:35:48 -0400

    now using django rest framework for the serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,16 @@
 import json
-from django.http import JsonResponse
-
 from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by('title').first()
+    """DRF API VIEW"""
+    instance = Product.objects.all().order_by('title').first()
     data = {}
-    if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(model_data)
+        data = ProductSerializer(instance).data
+    return Response(data)
